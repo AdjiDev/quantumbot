29c3f664e939f1c8bb9170eacc166869da55bc40
AdjiDev <153899174+AdjiDev@users.noreply.github.com>
2024-07-07 20:42:35 +0700

    init

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ Read MIT license before recode this script
 # configure token and other here
 
 # = [ setup owner ] = #
-token = '7046738742:AAEiT9cDbeD-7o4GG72a4g0E69A4oY9KR7I'  # get token from https://t.me/botfather
+token = 'your_bot_token'  # get token from https://t.me/botfather
 bot_version = 'v1_beta'
-owner_id = (6487149258)  # 
+owner_id = (6487149258)  # your telegram profile id 
 owner_name = '@rizkykianadji'
 owner_email = 'supportadjidev@gmail.com'
-owner_phone = '6283164470681'
+owner_phone = 'your phone number'
 timezone = 'Asia/Jakarta'
 
 # = [ keys ] = #
-gemini_key = 'AIzaSyCCN3O0Bz269RPe3WTse5oTbDsIM7Eavl8'
+gemini_key = 'your_gemini_keys' # has no effect i will update as soon possible
 openai_key = 'your_openai_key' # has no effect i will update as soon possible
 alicia_key = 'your_alicia_keys' # has no effect i will update as soon possible
 bing_cookie = 'your_bing_cookie' # has no effect i will update as soon possible
